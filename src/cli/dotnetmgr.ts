5e3e713ef06d26bbc1b752fc29f7af9da3044d1b
Roland Breitschaft <roland.breitschaft@x-company.de>
2019-01-07 23:17:49 +0100

    Add CLI

---
+++ src/cli/dotnetmgr.ts
@@ -0,0 +1,91 @@
+#! /usr/bin/env node
+
+/**
+ * Copyright (c) 2019 IT Solutions Roland Breitschaft <info@x-company.de>
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ *
+ * @Script: dotnetmgr.ts
+ * @Author: Roland Breitschaft
+ * @Email: roland.breitschaft@x-company.de
+ * @Create At: 2019-01-07 19:26:24
+ * @Last Modified By: Roland Breitschaft
+ * @Last Modified At: 2019-01-07 20:55:39
+ * @Description: This is description.
+ */
+
+import { Command } from 'commander';
+import { Info } from '../info';
+import { Updater } from '../updater/Updater';
+import { DotNetSettings } from '../app';
+import { Log } from '../helpers/Log';
+
+Updater.checkUpdate();
+
+const program = new Command();
+
+program
+    .version(Info.getProductVersion())
+    .description('dotnetmgr is a CLI tool whose purpose is to manage your .Net Core Solution and Project Settings.');
+
+program
+    .command('init')
+    .description('Generates DotNet Settings file foreach found Solution or Project File.')
+    .option('-s, --solution <file>', 'Specifies the Solution File.')
+    .option('-f, --force', 'Overwrites existing DotNet Settings Files. All existing Settings will lost.')
+    .option('-v, --verbose', 'Shows verbose Messages')
+    .action(async (options) => {
+
+        let level = 'warn';
+        if (options.verbose) {
+            level = 'verbose';
+
+        }
+        const solution = options.solution || undefined;
+
+        try {
+            const settings = new DotNetSettings(solution, {
+                UseAppVersionMgr: true,
+                LogLevel : level,
+            });
+            if (options.force) {
+                await settings.initSettings(true);
+            } else {
+                await settings.initSettings(false);
+            }
+        } catch (e) {
+            Log.critical(e);
+        }
+    });
+
+program
+    .command('update')
+    .description('Updates the DotNet Project Files with the defined Settings.')
+    .option('-s, --solution <file>', 'Specifies the Solution File.')
+    .option('-v, --verbose', 'Shows verbose Messages')
+    .action(async (options) => {
+
+        let level = 'warn';
+        if (options.verbose) {
+            level = 'verbose';
+        }
+        const solution = options.solution || undefined;
+
+        try {
+            const settings = new DotNetSettings(solution, {
+                UseAppVersionMgr: true,
+                LogLevel : level,
+            });
+            await settings.updateProjects();
+        } catch (e) {
+            Log.critical(e);
+        }
+    });
+
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+    process.exit();
+}
+program.parse(process.argv);
